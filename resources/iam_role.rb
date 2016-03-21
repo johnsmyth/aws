4460c6f5c11468ef05573e411664f23eabbe30e2
johnsmyth <fatjohnny@wastedbandwidth.net>
2016-03-21 15:30:56 -0500

    Update iam_role.rb
    Add FalseClass to  remove_policy_members in iam_role resource so it can be set to false without: 
    Option remove_policy_members must be a kind of [TrueClass]!  You passed false.

--- resources/iam_role.rb
+++ resources/iam_role.rb
@@ -5,7 +5,7 @@ attribute :role_name, kind_of: String, name_attribute: true
 attribute :path, kind_of: String, default: '/'
 attribute :assume_role_policy_document, kind_of: String, required: true
 attribute :policy_members, kind_of: Array, default: []
-attribute :remove_policy_members, kind_of: TrueClass, default: true
+attribute :remove_policy_members, kind_of: [TrueClass, FalseClass], default: true
 attribute :region, kind_of: String
 
 # aws credential attributes
